475efb2c20c45eb26b3aba547d3d29a2a197f1fe
MateuszBorzyszkowski <boszmate@protonmail.com>
2020-06-19 20:29:34 +0200

    Add clothing and footwear store app
    Project: clothingandfootwearstore
    
    Add a classes (User, Product, Cloth and Boots)  with constructors, getters and setters (price and productCount). Also added the overrided toString() method for each classes for better validation.

---
+++ src/main/java/com/mateusz/models/Boots.java
@@ -0,0 +1,29 @@
+package com.mateusz.models;
+
+public class Boots extends Product {
+    private int size;
+    private boolean isNatualSkin;
+
+    public Boots(int id, String productName, int price, int weight, String color, int productCount, int size, boolean isNatualSkin) {
+        super(id, productName, price, weight, color, productCount);
+        this.size = size;
+        this.isNatualSkin = isNatualSkin;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isNatualSkin() {
+        return isNatualSkin;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "Boots{" +
+                "size=" + size +
+                ", isNaturalSkin=" + isNatualSkin +
+                "}";
+    }
+}
